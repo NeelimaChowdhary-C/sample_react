c9506372324f72f7b29793b3a4f687adcc8acb91
NeelimaChowdhary-C <43788505+NeelimaChowdhary-C@users.noreply.github.com>
2019-07-17 02:23:10 -0700

    Updates

---
+++ precache-manifest.72445b356d6b553523c76103e6b3c3b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4be4cb57cc9631a66848b4f8ee6b911f",
+    "url": "/sample_react/index.html"
+  },
+  {
+    "revision": "db7c6d7ba6b8321529b3",
+    "url": "/sample_react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/sample_react/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "db7c6d7ba6b8321529b3",
+    "url": "/sample_react/static/js/main.d9c458e3.chunk.js"
+  },
+  {
+    "revision": "2f12a805db40fa6edbf8",
+    "url": "/sample_react/static/js/runtime~main.0a587ac5.js"
+  }
+]);
